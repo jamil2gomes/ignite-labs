9a1e922750874667a77d24d7d37ad25b5f9077db
ja1000 <jamil.lanister@hotmail.com>
2022-06-22 09:05:22 -0300

    Criaççao inicial dos componentes, configurações de fonte e cores no tailwind, importacao da logo svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql `
-  query{
-    lessons{
-      title,
-      id
-    }
-  }
-`;
+import Evento from "./pages/Evento";
 
 
-interface Lessons {
-  id:string;
-  title:string;
-}
 function App() {
 
-   const {data} = useQuery<{lessons:Lessons[]}>(GET_LESSONS_QUERY);
-
-  return (
+return (
     <div className="App">
-    
-        <ul>
-          {
-            data?.lessons.map(lesson => (
-              <li key={lesson.id}>{lesson.title}</li>
-            ))
-          }
-        </ul>
-       
+      <Evento/>
     </div>
   )
 }
---
+++ src/pages/Evento.tsx
@@ -0,0 +1,9 @@
+import Header from '../components/Header'
+
+export default function Evento() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
