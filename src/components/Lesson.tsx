f78a8c0150088bc060e3ad9ab3b1e8d30894605b
ja1000 <jamil.lanister@hotmail.com>
2022-06-24 11:31:05 -0300

    aula 04

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,7 +2,8 @@
 import {CheckCircle, Lock} from 'phosphor-react';
 import { format, isPast } from "date-fns";
 import ptBR from 'date-fns/locale/pt-BR';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface LessonProps {
   title: string;
@@ -13,6 +14,10 @@ interface LessonProps {
 
 export default function Lesson(props:LessonProps) {
   const isLessonAvailable = isPast(props.availableAt);
+  const {slug} =  useParams<{slug:string}>();
+
+  const isActive = slug === props.slug;
+
 
   const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR,
@@ -22,7 +27,12 @@ export default function Lesson(props:LessonProps) {
     <div>
       <Link to={`/event/lesson/${props.slug}`}className="group">
         <span className="text-gray-300">{availableDateFormatted}</span>
-        <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <div 
+        className={
+          classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500',{
+           'bg-green-500': isActive
+         })}
+         >
           <header className="flex items-center justify-between">
             
            {
@@ -47,7 +57,12 @@ export default function Lesson(props:LessonProps) {
 
           </header>
 
-          <strong className="block text-gray-200 mt-5">
+          <strong className={
+            classNames("block mt-5",{
+              'text-white': isActive,
+              'text-gray-200': !isActive
+            })
+          }>
             {props.title}
           </strong>
         </div>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o74o2s0xl801xxhk68e4gr/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers:{
+   'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 })
